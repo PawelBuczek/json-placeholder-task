6d4d9dd544fd8f424f72044bfa1ce84fc7182095
PawelBuczek <pawel.buczek@email.com>
2024-01-11 12:25:01 +0100

    moved static code to FileService

--- src/main/java/org/pbuczek/Main.java
+++ src/main/java/org/pbuczek/Main.java
@@ -1,31 +1,9 @@
 package org.pbuczek;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.pbuczek.exception.DuplicateIdException;
-import org.pbuczek.post.Post;
-import org.pbuczek.service.DataService;
 import org.pbuczek.service.FileService;
-
-import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        DataService dataService = new DataService(new ObjectMapper());
-        FileService fileService = new FileService();
-
-        try {
-            String jsonPosts = dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts");
-            List<Post> posts = dataService.mapJsonToPosts(jsonPosts);
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM(MMM)_dd_HH_mm_ss");
-            String folderPath = "results/posts_" + formatter.format(java.time.LocalDateTime.now());
-            fileService.createDirectory(folderPath);
-
-            dataService.savePostsToFiles(posts, folderPath);
-        } catch (DuplicateIdException | IOException e) {
-            e.printStackTrace();
-        }
+        FileService.downloadPostsToJsonFiles();
     }
 }
--- src/main/java/org/pbuczek/service/FileService.java
+++ src/main/java/org/pbuczek/service/FileService.java
@@ -1,18 +1,40 @@
 package org.pbuczek.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.pbuczek.exception.DuplicateIdException;
+import org.pbuczek.post.Post;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class FileService {
-    public void createDirectory(String folderPath) throws IOException {
+    public static void createDirectory(String folderPath) throws IOException {
         try {
             Path path = Paths.get(folderPath);
             Files.createDirectories(path);
 
         } catch (IOException e) {
             throw new IOException("Failed to create directory:" + folderPath + e.getMessage());
+        }
+    }
+
+    public static void downloadPostsToJsonFiles() {
+        DataService dataService = new DataService(new ObjectMapper());
+        try {
+            String jsonPosts = dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts");
+            List<Post> posts = dataService.mapJsonToPosts(jsonPosts);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM(MMM)_dd_HH_mm_ss");
+            String folderPath = "results/posts_" + formatter.format(java.time.LocalDateTime.now());
+            createDirectory(folderPath);
+
+            dataService.savePostsToFiles(posts, folderPath);
+        } catch (DuplicateIdException | IOException e) {
+            e.printStackTrace();
         }
     }
 }
