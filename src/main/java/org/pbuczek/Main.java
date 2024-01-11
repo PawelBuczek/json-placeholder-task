bb6e57042988334307558ab508ea26fd3c62bc3b
PawelBuczek <pawel.buczek@email.com>
2024-01-11 11:48:40 +0100

    got json post objects from url

--- src/main/java/org/pbuczek/Main.java
+++ src/main/java/org/pbuczek/Main.java
@@ -1,7 +1,18 @@
 package org.pbuczek;
 
+import org.pbuczek.service.DataService;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            DataService dataService = new DataService();
+            String jsonPosts = dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts");
+            System.out.println(jsonPosts);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/pbuczek/service/DataService.java
+++ src/main/java/org/pbuczek/service/DataService.java
@@ -1,4 +1,17 @@
 package org.pbuczek.service;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class DataService {
+    @SuppressWarnings("SameParameterValue")
+    public String getJsonFromUrlAddress(String urlAddress) throws IOException {
+        try (InputStream inputStream = new URL(urlAddress).openStream()) {
+            return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        }
+    }
 }
