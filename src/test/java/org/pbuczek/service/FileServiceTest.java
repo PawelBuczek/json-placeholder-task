6d0baeff5793b728524ca7d1ec5e5aa0d0ab331f
PawelBuczek <pawel.buczek@email.com>
2024-01-11 12:40:54 +0100

    added assertion that file exists

--- src/test/java/org/pbuczek/service/FileServiceTest.java
+++ src/test/java/org/pbuczek/service/FileServiceTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 class FileServiceTest {
@@ -20,8 +22,10 @@ class FileServiceTest {
 
         FileService.setDataService(dataService);
 
-        FileService.downloadPostsToJsonFiles();
+        String folderPath = FileService.downloadPostsToJsonFiles();
 
+        File file = new File(folderPath + "/1.json");
+        assertTrue(file.exists());
         // assertions to make - is file created and are its contents what we expect
     }
 }
