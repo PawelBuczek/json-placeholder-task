e299cb45948b1dd3bd1ad47392d7df78027f71e2
PawelBuczek <pawel.buczek@email.com>
2024-01-11 13:21:49 +0100

    added shouldSaveThreeJsonFilesWhenGivenThreePosts test case

--- src/test/java/org/pbuczek/service/FileServiceTest.java
+++ src/test/java/org/pbuczek/service/FileServiceTest.java
@@ -2,6 +2,7 @@ package org.pbuczek.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -13,24 +14,61 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 class FileServiceTest {
+    private static String folderPath = "";
+
+    @AfterEach
+    void tearDown() throws IOException {
+        FileUtils.deleteDirectory(new File(folderPath));
+    }
 
     @Test
     void shouldSaveJsonFileWhenGivenOnePost() throws IOException {
         // given
         String expectedFileContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title\",\"body\":\"Test Body\"}";
-        String mockedJsonResponse = "[{\"userId\": 1, \"id\": 1, \"title\": \"Test Title\", \"body\": \"Test Body\"}]";
-        DataService dataService = Mockito.spy(new DataService(new ObjectMapper()));
-        when(dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts"))
-                .thenReturn(mockedJsonResponse);
         FileService fileService = new FileService();
-        fileService.setDataService(dataService);
+        mockGetJsonFromUrlAddressMethod(fileService,
+                "[{\"userId\": 1, \"id\": 1, \"title\": \"Test Title\", \"body\": \"Test Body\"}]");
 
         // when
-        String folderPath = fileService.downloadPostsToJsonFiles();
+        folderPath = fileService.downloadPostsToJsonFiles();
 
         // then
         File file = new File(folderPath + "/1.json");
         assertTrue(file.exists());
         assertEquals(expectedFileContents, FileUtils.readFileToString(file, "utf-8"));
+    }
+
+    @Test
+    void shouldSaveThreeJsonFilesWhenGivenThreePosts() throws IOException {
+        // given
+        String expectedFileOneContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title1\",\"body\":\"Test Body\"}";
+        String expectedFileTwoContents = "{\"userId\":1,\"id\":2,\"title\":\"Test Title2\",\"body\":\"Test Body\"}";
+        String expectedFileThreeContents = "{\"userId\":2,\"id\":3,\"title\":\"Test Title3\",\"body\":\"Test Body\"}";
+        FileService fileService = new FileService();
+        mockGetJsonFromUrlAddressMethod(fileService,
+                "[{\"userId\": 1, \"id\": 1, \"title\": \"Test Title1\", \"body\": \"Test Body\"}," +
+                        "{\"userId\": 1, \"id\": 2, \"title\": \"Test Title2\", \"body\": \"Test Body\"}," +
+                        "{\"userId\": 2, \"id\": 3, \"title\": \"Test Title3\", \"body\": \"Test Body\"}]");
+
+        // when
+        folderPath = fileService.downloadPostsToJsonFiles();
+
+        // then
+        assertPostFileIsCorrect(folderPath + "/1.json", expectedFileOneContents);
+        assertPostFileIsCorrect(folderPath + "/2.json", expectedFileTwoContents);
+        assertPostFileIsCorrect(folderPath + "/3.json", expectedFileThreeContents);
+    }
+
+    private void assertPostFileIsCorrect(String filePath, String expectedFileContents) throws IOException {
+        File file = new File(filePath);
+        assertTrue(file.exists());
+        assertEquals(expectedFileContents, FileUtils.readFileToString(file, "utf-8"));
+    }
+
+    private void mockGetJsonFromUrlAddressMethod(FileService fileService, String mockedJsonResponse) throws IOException {
+        DataService dataService = Mockito.spy(new DataService(new ObjectMapper()));
+        when(dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts"))
+                .thenReturn(mockedJsonResponse);
+        fileService.setDataService(dataService);
     }
 }
