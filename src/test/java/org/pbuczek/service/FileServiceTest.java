ebc4d0bc64e119cda1602b38fbb229cb740c4a16
PawelBuczek <pawel.buczek@email.com>
2024-01-11 13:54:08 +0100

    added empty and null test cases

--- src/test/java/org/pbuczek/service/FileServiceTest.java
+++ src/test/java/org/pbuczek/service/FileServiceTest.java
@@ -3,18 +3,33 @@ package org.pbuczek.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class FileServiceTest {
+    private final static ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final static PrintStream originalErr = System.err;
     private static String folderPath = "";
+
+    @BeforeAll
+    public static void setUpStreams() {
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterAll
+    public static void restoreStreams() {
+        System.setErr(originalErr);
+    }
 
     @AfterEach
     @SneakyThrows
@@ -25,7 +40,6 @@ class FileServiceTest {
     @Test
     void shouldSaveJsonFileWhenGivenOnePost() {
         // given
-        String expectedFileContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title\",\"body\":\"Test Body\"}";
         FileService fileService = new FileService();
         mockGetJsonFromUrlAddressMethod(fileService,
                 "[{\"userId\": 1, \"id\": 1, \"title\": \"Test Title\", \"body\": \"Test Body\"}]");
@@ -34,15 +48,13 @@ class FileServiceTest {
         folderPath = fileService.downloadPostsToJsonFiles();
 
         // then
+        String expectedFileContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title\",\"body\":\"Test Body\"}";
         assertPostFileIsCorrect(folderPath + "/1.json", expectedFileContents);
     }
 
     @Test
     void shouldSaveThreeJsonFilesWhenGivenThreePosts() {
         // given
-        String expectedFileOneContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title1\",\"body\":\"Test Body\"}";
-        String expectedFileTwoContents = "{\"userId\":1,\"id\":2,\"title\":\"Test Title2\",\"body\":\"Test Body\"}";
-        String expectedFileThreeContents = "{\"userId\":2,\"id\":3,\"title\":\"Test Title3\",\"body\":\"Test Body\"}";
         FileService fileService = new FileService();
         mockGetJsonFromUrlAddressMethod(fileService,
                 "[{\"userId\": 1, \"id\": 1, \"title\": \"Test Title1\", \"body\": \"Test Body\"}," +
@@ -53,9 +65,41 @@ class FileServiceTest {
         folderPath = fileService.downloadPostsToJsonFiles();
 
         // then
+        String expectedFileOneContents = "{\"userId\":1,\"id\":1,\"title\":\"Test Title1\",\"body\":\"Test Body\"}";
+        String expectedFileTwoContents = "{\"userId\":1,\"id\":2,\"title\":\"Test Title2\",\"body\":\"Test Body\"}";
+        String expectedFileThreeContents = "{\"userId\":2,\"id\":3,\"title\":\"Test Title3\",\"body\":\"Test Body\"}";
         assertPostFileIsCorrect(folderPath + "/1.json", expectedFileOneContents);
         assertPostFileIsCorrect(folderPath + "/2.json", expectedFileTwoContents);
         assertPostFileIsCorrect(folderPath + "/3.json", expectedFileThreeContents);
+    }
+
+    @Test
+    void shouldPrintStackTraceWhenResponseIsEmpty() {
+        // given
+        FileService fileService = new FileService();
+        mockGetJsonFromUrlAddressMethod(fileService,
+                "");
+
+        // when
+        fileService.downloadPostsToJsonFiles();
+
+        // then
+        assert (errContent.toString().contains(
+                "com.fasterxml.jackson.databind.exc.MismatchedInputException: No content to map due to end-of-input"));
+    }
+
+    @Test
+    void shouldPrintStackTraceWhenResponseIsNull() {
+        // given
+        FileService fileService = new FileService();
+        mockGetJsonFromUrlAddressMethod(fileService,
+                null);
+
+        // when
+        Exception exception = assertThrows(IllegalArgumentException.class, fileService::downloadPostsToJsonFiles);
+
+        // then
+        assert(exception.getMessage().contains("argument \"content\" is null"));
     }
 
     @SneakyThrows
