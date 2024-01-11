29eb6a949d89290da7bdde8ba7f349cc355145a1
PawelBuczek <pawel.buczek@email.com>
2024-01-11 12:06:18 +0100

    mapped json to List<Post>

--- src/main/java/org/pbuczek/Main.java
+++ src/main/java/org/pbuczek/Main.java
@@ -1,17 +1,23 @@
 package org.pbuczek;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.pbuczek.exception.DuplicateIdException;
+import org.pbuczek.post.Post;
 import org.pbuczek.service.DataService;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             DataService dataService = new DataService();
             String jsonPosts = dataService.getJsonFromUrlAddress("https://jsonplaceholder.typicode.com/posts");
-            System.out.println(jsonPosts);
+            ObjectMapper mapper = new ObjectMapper();
+            List<Post> posts = dataService.mapJsonToPosts(mapper, jsonPosts);
+            System.out.println(posts);
 
-        } catch (IOException e) {
+        } catch (DuplicateIdException | IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/org/pbuczek/service/DataService.java
+++ src/main/java/org/pbuczek/service/DataService.java
@@ -1,11 +1,19 @@
 package org.pbuczek.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
+import org.pbuczek.exception.DuplicateIdException;
+import org.pbuczek.post.Post;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DataService {
     @SuppressWarnings("SameParameterValue")
@@ -13,5 +21,20 @@ public class DataService {
         try (InputStream inputStream = new URL(urlAddress).openStream()) {
             return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
         }
+    }
+
+    public List<Post> mapJsonToPosts(ObjectMapper mapper, String jsonPosts) throws DuplicateIdException, JsonProcessingException {
+        List<Post> posts = mapper.readValue(jsonPosts, new TypeReference<>() {
+        });
+
+        Set<Integer> setOfPostIds = posts.stream()
+                .map(Post::getId)
+                .collect(Collectors.toSet());
+
+        if (setOfPostIds.size() < posts.size()) {
+            throw new DuplicateIdException("Returned jsonObject contains duplicated post ids");
+        }
+
+        return posts;
     }
 }
