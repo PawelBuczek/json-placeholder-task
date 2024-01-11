1a3337dba5e51e420676a5741161c78629f8d292
PawelBuczek <pawel.buczek@email.com>
2024-01-11 11:33:34 +0100

    added Post class

--- src/main/java/org/pbuczek/post/Post.java
+++ src/main/java/org/pbuczek/post/Post.java
@@ -1,4 +1,15 @@
 package org.pbuczek.post;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
 }
