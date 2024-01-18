ac286030b3e92cae768888fac6b797376f82e626
PawelBuczek <pawel.buczek@email.com>
2024-01-18 16:21:23 +0100

    updated logic to group posts by user

--- src/main/java/org/pbuczek/service/FileService.java
+++ src/main/java/org/pbuczek/service/FileService.java
@@ -10,10 +10,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FileService {
     private DataService dataService = new DataService(new ObjectMapper());
     public static final String JSON_PLACEHOLDER_POSTS_URL_ADDRESS = "https://jsonplaceholder.typicode.com/posts";
+    public static final int MAX_NUMBER_OF_POSTS_PER_USER = 3;
 
     public void setDataService(DataService dataService) {
         this.dataService = dataService;
@@ -32,19 +35,28 @@ public class FileService {
     public String downloadPostsToJsonFiles() {
         String folderPath = "";
         try {
-            String jsonPosts = dataService.getJsonFromUrlAddress(JSON_PLACEHOLDER_POSTS_URL_ADDRESS);
-            List<Post> posts = dataService.mapJsonToPosts(jsonPosts);
-
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM(MMM)_dd_HH_mm_ss");
             folderPath = "results/posts_" + formatter.format(java.time.LocalDateTime.now());
             createDirectory(folderPath);
+            String jsonPosts = dataService.getJsonFromUrlAddress(JSON_PLACEHOLDER_POSTS_URL_ADDRESS);
+            List<Post> posts = dataService.mapJsonToPosts(jsonPosts);
+            Map<Integer, List<Post>> postsByUser = posts.stream()
+                    .collect(Collectors.groupingBy(Post::getUserId));
 
-            dataService.savePostsToFiles(posts, folderPath);
+            String finalFolderPath = folderPath;
+            postsByUser
+                    .forEach((userId, userPosts) -> {
+                        try {
+                            dataService.savePostsToFiles(userPosts.stream().limit(MAX_NUMBER_OF_POSTS_PER_USER).toList(),
+                                    userId, finalFolderPath);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
         } catch (DuplicateIdException | IOException e) {
             //probably would be better to log with @SLF4J or something
             e.printStackTrace();
         }
-
         return folderPath;
     }
 }
